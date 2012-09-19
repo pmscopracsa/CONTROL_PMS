562faaa55727fd306ae032e0e81ec914526db3fe
Miguel Angel Alcantara Ayre <miguel.alc@gmail.com>
2012-09-19 10:24:26 -0500

    decode none

--- bl/ComboBoxTipos.php
+++ bl/ComboBoxTipos.php
@@ -54,7 +54,7 @@ class ComboBoxTipos extends ComboBoxSql
             
             while ($tipodocumento = parent::fetch_assoc($consulta)) {
                 $id = $tipodocumento['id'];
-                $descripcion = utf8_decode($tipodocumento['descripcion']);
+                $descripcion = $tipodocumento['descripcion'];
                 $tiposdocumento[$id] = $descripcion;
             }
             return $tiposdocumento;
